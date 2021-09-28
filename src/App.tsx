2adf86d32d653e32a6466d10f443ebfa7e8cce56
Tiffany Zhang <tiff@Tiffanys-MacBook-Pro.local>
2021-09-28 14:27:23 -0700

    Replace some boilerplate with more custom stuff

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,9 @@ export default function App() {
   const [count, setCount] = useState(0);
   return (
     <div className={styles.container}>
-      <h2>React minimal starter kit</h2>
+      <h2>Corp clicker</h2>
       <p>
-        This is a simple starter kit to help you bootstrap your React project
-        with few frills.
+        Click to get respect.
       </p>
       <button type="button" onClick={() => setCount(count + 1)}>
         Increase cat count: {count}
