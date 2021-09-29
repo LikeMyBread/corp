d4ff7a2f95157392dcb10b5543df3fe02e9d2617
Tiffany Zhang <tiff@Tiffanys-MacBook-Pro.local>
2021-09-29 14:55:31 -0700

    Updates

---
+++ build/main.3c6dc976ec9537b80e55.hot-update.js
@@ -0,0 +1,125 @@
+"use strict";
+self["webpackHotUpdate"]("main",{
+
+/***/ "./App.tsx":
+/*!*****************!*\
+  !*** ./App.tsx ***!
+  \*****************/
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": function() { return /* binding */ App; }
+/* harmony export */ });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "../node_modules/react/index.js");
+/* harmony import */ var _App_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./App.css */ "./App.css");
+/* harmony import */ var _App_css__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_App_css__WEBPACK_IMPORTED_MODULE_1__);
+/* module decorator */ module = __webpack_require__.hmd(module);
+(function () {
+  var enterModule = typeof reactHotLoaderGlobal !== 'undefined' ? reactHotLoaderGlobal.enterModule : undefined;
+  enterModule && enterModule(module);
+})();
+
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function _iterableToArrayLimit(arr, i) { var _i = arr == null ? null : typeof Symbol !== "undefined" && arr[Symbol.iterator] || arr["@@iterator"]; if (_i == null) return; var _arr = []; var _n = true; var _d = false; var _s, _e; try { for (_i = _i.call(arr); !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
+var __signature__ = typeof reactHotLoaderGlobal !== 'undefined' ? reactHotLoaderGlobal.default.signature : function (a) {
+  return a;
+};
+
+
+
+var startTime = Date.now();
+function App() {
+  var _useState = (0,react__WEBPACK_IMPORTED_MODULE_0__.useState)(0),
+      _useState2 = _slicedToArray(_useState, 2),
+      respect = _useState2[0],
+      setRespect = _useState2[1];
+
+  var _useState3 = (0,react__WEBPACK_IMPORTED_MODULE_0__.useState)(startTime),
+      _useState4 = _slicedToArray(_useState3, 2),
+      currentTime = _useState4[0],
+      setCurrentTime = _useState4[1];
+
+  (0,react__WEBPACK_IMPORTED_MODULE_0__.useEffect)(function () {
+    var timer;
+
+    var updateTimer = function updateTimer() {
+      setCurrentTime(Date.now());
+      timer = setTimeout(updateTimer, 1000 - Date.now() % 1000);
+    };
+
+    timer = setTimeout(updateTimer, 1000 - Date.now() % 1000);
+    return function () {
+      clearTimeout(timer);
+    };
+  });
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+    className: (_App_css__WEBPACK_IMPORTED_MODULE_1___default().container)
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h2", null, "Corp clicker"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("p", null, "Click to get respect. ", /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("i", {
+    className: "far fa-lightbulb"
+  })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("p", null, "Time since start time: ", currentTime - startTime), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("button", {
+    type: "button",
+    onClick: function onClick() {
+      return setRespect(respect + 1);
+    }
+  }, "Increase cat respect: ", respect), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(CatRespecter, {
+    respect: respect
+  }));
+}
+
+__signature__(App, "useState{[respect, setRespect](0)}\nuseState{[currentTime, setCurrentTime](startTime)}\nuseEffect{}");
+
+function CatRespecter(_ref) {
+  var respect = _ref.respect;
+  var cats = [];
+
+  for (var i = 0; i < respect; i++) {
+    cats.push('🐱');
+  }
+
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(react__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, cats);
+}
+
+;
+
+(function () {
+  var reactHotLoader = typeof reactHotLoaderGlobal !== 'undefined' ? reactHotLoaderGlobal.default : undefined;
+
+  if (!reactHotLoader) {
+    return;
+  }
+
+  reactHotLoader.register(startTime, "startTime", "/Users/tiff/Projects/corp/src/App.tsx");
+  reactHotLoader.register(App, "App", "/Users/tiff/Projects/corp/src/App.tsx");
+  reactHotLoader.register(CatRespecter, "CatRespecter", "/Users/tiff/Projects/corp/src/App.tsx");
+})();
+
+;
+
+(function () {
+  var leaveModule = typeof reactHotLoaderGlobal !== 'undefined' ? reactHotLoaderGlobal.leaveModule : undefined;
+  leaveModule && leaveModule(module);
+})();
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ !function() {
+/******/ 	__webpack_require__.h = function() { return "03c548d22a790f13bbc1"; }
+/******/ }();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=main.3c6dc976ec9537b80e55.hot-update.js.map
---
+++ build/main.f676d496ba242666f791.hot-update.js
@@ -0,0 +1,22 @@
+self["webpackHotUpdate"]("main",{
+
+/***/ "./App.tsx":
+/*!*****************!*\
+  !*** ./App.tsx ***!
+  \*****************/
+/***/ (function() {
+
+throw new Error("Module build failed (from ../node_modules/babel-loader/lib/index.js):\nSyntaxError: /Users/tiff/Projects/corp/src/App.tsx: Unexpected token, expected \"jsxTagEnd\" (27:6)\n\n\u001b[0m \u001b[90m 25 |\u001b[39m     \u001b[33m<\u001b[39m\u001b[33mdiv\u001b[39m className\u001b[33m=\u001b[39m{styles\u001b[33m.\u001b[39mcontainer}\u001b[33m>\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 26 |\u001b[39m       \u001b[33m<\u001b[39m\u001b[33mh2\u001b[39m\u001b[33m>\u001b[39m\u001b[33mCorp\u001b[39m clicker\u001b[33m!\u001b[39m\u001b[33m<\u001b[39m\u001b[33m/\u001b[39m\u001b[33mh2\u001b[39m\u001b[0m\n\u001b[0m\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 27 |\u001b[39m       \u001b[33m<\u001b[39m\u001b[33mp\u001b[39m\u001b[33m>\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m    |\u001b[39m       \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 28 |\u001b[39m         \u001b[33mClick\u001b[39m to \u001b[36mget\u001b[39m respect\u001b[33m.\u001b[39m \u001b[33m<\u001b[39m\u001b[33mi\u001b[39m className\u001b[33m=\u001b[39m\u001b[32m\"far fa-lightbulb\"\u001b[39m \u001b[33m/\u001b[39m\u001b[33m>\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 29 |\u001b[39m       \u001b[33m<\u001b[39m\u001b[33m/\u001b[39m\u001b[33mp\u001b[39m\u001b[33m>\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 30 |\u001b[39m       \u001b[33m<\u001b[39m\u001b[33mp\u001b[39m\u001b[33m>\u001b[39m\u001b[33mTime\u001b[39m since start time\u001b[33m:\u001b[39m {currentTime \u001b[33m-\u001b[39m startTime}\u001b[33m<\u001b[39m\u001b[33m/\u001b[39m\u001b[33mp\u001b[39m\u001b[33m>\u001b[39m\u001b[0m\n    at Object._raise (/Users/tiff/Projects/corp/node_modules/@babel/parser/lib/index.js:510:17)\n    at Object.raiseWithData (/Users/tiff/Projects/corp/node_modules/@babel/parser/lib/index.js:503:17)\n    at Object.raise (/Users/tiff/Projects/corp/node_modules/@babel/parser/lib/index.js:464:17)\n    at Object.unexpected (/Users/tiff/Projects/corp/node_modules/@babel/parser/lib/index.js:3368:16)\n    at Object.expect (/Users/tiff/Projects/corp/node_modules/@babel/parser/lib/index.js:3342:28)\n    at Object.jsxParseClosingElementAt (/Users/tiff/Projects/corp/node_modules/@babel/parser/lib/index.js:7167:10)\n    at Object.jsxParseElementAt (/Users/tiff/Projects/corp/node_modules/@babel/parser/lib/index.js:7186:37)\n    at Object.jsxParseElementAt (/Users/tiff/Projects/corp/node_modules/@babel/parser/lib/index.js:7190:32)\n    at Object.jsxParseElement (/Users/tiff/Projects/corp/node_modules/@babel/parser/lib/index.js:7248:17)\n    at Object.parseExprAtom (/Users/tiff/Projects/corp/node_modules/@babel/parser/lib/index.js:7255:19)");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ !function() {
+/******/ 	__webpack_require__.h = function() { return "fff9f112f28bdcc9b2c9"; }
+/******/ }();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=main.f676d496ba242666f791.hot-update.js.map
